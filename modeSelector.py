17b04fe11cc1fd6101a0f2f9481ccf5890cedeed
Lucidreline <techservs@pm.me>
2020-12-30 02:13:20 -0800

    Led lights will reflect what the current weather is.

--- modeSelector.py
+++ modeSelector.py
@@ -53,11 +53,40 @@ class ModeSelector:
             lightTemps.append(firstLightBrightness +
                               (i * config.lightTemperatureInterval))
             # if comfortableTemp = 65 & interval = 10, then lightTemps = [45, 55, 65, 75, 85]
+        updatedLights = [0, 0, 0, 0, 0]
 
-        updatedLights = [0, 50, 0, 100, 0]  # test numbers
+        if currentTemperature <= lightTemps[0]:
+            updatedLights[0] = 100
+        elif currentTemperature >= lightTemps[len(lightTemps) - 1]:
+            updatedLights[len(updatedLights) - 1] = 100
+        else:
+            for i in range(1, len(lightTemps)):  # starts index at 1
+                if currentTemperature <= lightTemps[i]:
+
+                    # lets say the previous light temp was 55, then temperature is 60, and the current light temp is 65
+                    # we take the previous light temp (55) and subtract that from the other two values, so
+                    # we get a temperature of 5, and a current light temp of 10
+                    # then divide the temperature by current light temp (5/10), and you get your current light percentage
+                    # multiply it by 100 so that it goes from decimal to percent (0.5 -> 50)
+                    # to get the previous light percent brightness, subtract 100 - the previous percentage (50)
+
+                    subtractedCurrentLightTemp = lightTemps[i] - \
+                        lightTemps[i-1]
+                    subtractedCurrentTemperature = currentTemperature - \
+                        lightTemps[i-1]
+
+                    currentLightBrightness = (
+                        subtractedCurrentTemperature / subtractedCurrentLightTemp) * 100
+                    previousLightBrightness = 100 - currentLightBrightness
+
+                    updatedLights[i] = currentLightBrightness
+                    updatedLights[i - 1] = previousLightBrightness
+
+                    break
+
         self.lights.updateLights(updatedLights)
 
-        # light temperatures: 45 55 65 75 85
+        # test light temperatures: 45 55 65 75 85
         # tests cases:
         # 65 -> [0 0 100 0 0]  | if the degrees matches a light tempertature exactly, that light will be at 100% brightness
         # 70 -> [0 0 50 50 0]  | if the degrees is in the middle of two lights, the two lights will be at 50%
